3354b2a1e2c313ec5fa672c7c9341f630d36cf06
Nick Rimmer <nick.rimmer.2009@gmail.com>
2021-07-22 14:44:52 +0100

    Added new sections for NSG

--- ATA/main.tf
+++ ATA/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_public_ip" "rg" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Dynamic"
-  sku="basic"
+  sku                 = "basic"
 }
 resource "azurerm_network_interface" "rg" {
   count               = 2
@@ -44,10 +44,36 @@ resource "azurerm_network_interface" "rg" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.rg.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = "${element(azurerm_public_ip.rg.*.id, count.index + 1)}"
+    public_ip_address_id          = element(azurerm_public_ip.rg.*.id, count.index + 1)
   }
 
 }
+resource "azurerm_network_security_group" "rg" {
+  name                = "AZ-VM-NSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  # Security rule can also be defined with resource azurerm_network_security_rule, here just defining it inline.
+  security_rule {
+    name                       = "Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  tags = {
+    environment = "NSG"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "rg_nsg_association" {
+  subnet_id                 = azurerm_subnet.rg.id
+  network_security_group_id = azurerm_network_security_group.rg.id
+}
+
 resource "azurerm_windows_virtual_machine" "rg" {
   count               = 2
   name                = "AZ-VM-00-${count.index}"
@@ -55,7 +81,7 @@ resource "azurerm_windows_virtual_machine" "rg" {
   location            = azurerm_resource_group.rg.location
   size                = "Standard_F2"
   admin_username      = "adminata"
-  admin_password      = "P$T^r@be%9Mm"
+  admin_password      = "password"
   network_interface_ids = [
     azurerm_network_interface.rg.*.id[count.index],
   ]
